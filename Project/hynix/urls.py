bd34b8bbd3516918cf8b6f58e201c9e7f3709cd3
jungwoo Lee <135038257+grandjwl@users.noreply.github.com>
2023-08-19 00:05:58 +0900

    Delete Project directory

--- Project/hynix/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.main, name="main"),
-    path('simulator/',views.simulation,name='simulator'),
-    path('mlc/',views.lifecycle,name='mlc'),
-]
-
