e3da1dff8b8ac10f04ef187b7be74801d2e605e6
GWC <cku7808@naver.com>
2023-08-30 15:45:36 +0900

    final before style

--- hynix/admin.py
+++ hynix/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import PreprocessedCSV, Prediction_complete #Wsimulation, WLifecycle
+from .models import PreprocessedCSV, Prediction_complete ,Wsimulation, WLifecycle
 
 # 각 모델을 관리자 사이트에 등록합니다.
 admin.site.register(PreprocessedCSV)
 admin.site.register(Prediction_complete)
-# admin.site.register(Wsimulation)
-# admin.site.register(WLifecycle)
+admin.site.register(Wsimulation)
+admin.site.register(WLifecycle)
--- hynix/models.py
+++ hynix/models.py
@@ -13,9 +13,6 @@ class PreprocessedCSV(dj_models.Model):
     name = dj_models.CharField(max_length=100)
     data = dj_models.FileField(upload_to='input_csvs/')
     
-    def __str__(self):
-        return self.name
-    
 # 3. 앙상블 완료된 'test.csv+앙상블prediction' df를 csv로 저장하는 테이블
 class Prediction_complete(dj_models.Model):
     ID = dj_models.AutoField(primary_key=True)
@@ -33,7 +30,7 @@ class Wsimulation(dj_models.Model):
 # 5. lifecycle 페이지에서 필요한거 저장 및 사용하는 테이블 
 class WLifecycle(dj_models.Model): # lifecycle은 맨아래 행만 필요  
     test_csv = dj_models.FileField(upload_to='input_csvs/') # 업로드된 test.csv 원본
-    Lot_ID = dj_models.CharField(max_length=100)
+    Lot_ID = dj_models.CharField(max_length=100) # simulation 페이지에서 id 저장
     avg_value = dj_models.FloatField(null=True, blank=True)  # 평균값 저장을 위한 필드 추가
     real = dj_models.FloatField(null=True, blank=True)
-    real_input_time = dj_models.DateTimeField(auto_now_add=True)
+    real_input_time = dj_models.DateTimeField(null=True, blank=True)
